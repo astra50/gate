c4c9dafbff582887a3371216fde4e55f628391ce
Konstantin Grachev <me@grachevko.ru>
2020-05-08 15:49:27 +0300

    UserId

---
+++ config/identifiers.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    App\Domain\User\UserId::class => ['user_id'],
+];
---
+++ src/Domain/User/UserId.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\User;
+
+use App\Infrastructure\Identifier\Identifier;
+
+/**
+ * @psalm-immutable
+ */
+final class UserId extends Identifier
+{
+}
