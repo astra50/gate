7a339a6ee436162e8f2c7f6648f0725a52adf5a9
Kirill Sidorov <kirillsidorov@gmail.com>
2021-12-14 16:31:56 +0300

    Init react based gate button

---
+++ assets/js/components/gate-button/gate-button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GateButton = () => {
+  return <div>Gate Button</div>
+}
+
+export default GateButton;
---
+++ assets/js/sections/sec-button.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ButtonPage from "../components/button-page/button-page";
+
+ReactDOM.render(<ButtonPage/>, document.getElementById('js-button'))
