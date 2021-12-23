ff49ec9fdf147447ea0f8526b84031c53130d4ba
Kirill Sidorov <kirillsidorov@gmail.com>
2021-12-23 17:19:12 +0300

    integrate alerts

--- assets/js/components/button/progress-bar/progress-bar.js
+++ assets/js/components/button/progress-bar/progress-bar.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import './progress-bar.less'
+import React from "react";
+import "./progress-bar.less";
 
-const ProgressBar = ({rotate, colors: {bar: backgroundColor, shadow}, className=''}) => {
+const ProgressBar = ({
+  rotate,
+  colors: { bar: backgroundColor, shadow },
+  className = "",
+}) => {
   const lineStyle = {
-    transform: `rotate(${rotate}deg)`, 
+    transform: `rotate(${rotate}deg)`,
     backgroundColor,
-  }
+  };
   const buttonStyle = {
-    boxShadow: `${ shadow } 0px 0px 20px`
-  }
+    boxShadow: `${shadow} 0px 0px 20px`,
+  };
   return (
-      <div className={`progress-bar ${className} ${rotate > 180 && 'gt50'}`} style={buttonStyle}>
-        <div className="_line" style={lineStyle} />
-        <div className="_r-mask" style={rotate > 180? { backgroundColor } : {}} />
-      </div>
-    )
-}
+    <div
+      className={`progress-bar ${className} ${rotate > 180 && "gt50"}`}
+      style={buttonStyle}
+    >
+      <div className='_line' style={lineStyle} />
+      <div
+        className='_r-mask'
+        style={rotate > 180 ? { backgroundColor } : {}}
+      />
+    </div>
+  );
+};
 
 export default ProgressBar;
