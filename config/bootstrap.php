bd281427f5cefe6188bc7bc8ad644453280bb761
Konstantin Grachev <me@grachevko.ru>
2022-04-27 22:10:32 +0300

    Check .env is exists before load

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,7 +12,7 @@ if (!class_exists(Dotenv::class)) {
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
     (new Dotenv(false))->populate($env);
-} else {
+} elseif (file_exists($path = dirname(__DIR__).'/.env')) {
     // load all the .env files
     (new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
 }
